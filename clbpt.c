1515766bb4568e7f09f39301ad0839aac88b135e
mayone <mayone321@gmail.com>
2015-01-27 14:19:23 +0800

    nothing

--- clbpt.c
+++ clbpt.c
@@ -5,6 +5,10 @@
 #include "clbpt.h"
  
 int clbptCreatePlatform(clbpt_platform dst_platform, cl_context context)
-{
-	
-}
+{}
+
+int clbptCreateTree(clbpt_tree dst_tree, clbpt_platform platform, const int degree, const size_t record_size)
+{}
+
+int clbptReleaseTree(clbpt_tree tree)
+{}
