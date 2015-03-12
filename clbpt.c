dbc4611e5ce02af6ffa3934120f0be79af6ee94e
CLBplustree <fhsh810305@gmail.com>
2015-03-12 16:18:27 +0800

    format

--- clbpt.c
+++ clbpt.c
@@ -69,13 +69,13 @@ int clbptCreatePlatform(
     if(err != CL_SUCCESS || dst_platform->queue == 0)                           {
         clReleaseContext(context)                                               ;
         return err                                                              ;}
-	return CLBPT_SUCCESS                                                        ;}
+	return CLBPT_SUCCESS                                                    ;}
 
 int clbptCreateTree(	
             clbpt_tree dst_tree, 
 			clbpt_platform platform, 
 			const int degree, 
-			const size_t record_size)				                            {
+			const size_t record_size)				{
     int err                                                                     ;
     pthread_t thread                                                            ;
     dst_tree = malloc(sizeof(struct _clbpt_tree))                               ;
@@ -89,9 +89,9 @@ int clbptCreateTree(
     dst_tree->execute_result_buf = calloc(sizeof(void *),CLBPT_BUF_SIZE)        ;
     dst_tree->fetch_buf_index = 0                                               ;
     if( (err = pthread_mutex_init(&(dst_tree->buffer_mutex),NULL)) != 0)
-	    return err								                                ;
+	    return err								;
     if( (err = pthread_mutex_init(&(dst_tree->loop_mutex),NULL)) != 0) 
-	    return err								                                ;
+	    return err								;
     _clbptInitialize(dst_tree)                                                  ;
     pthread_create(&thread,NULL,_clbptHandler,dst_tree)                         ;
     return CLBPT_SUCCESS                                                        ;}
@@ -108,13 +108,13 @@ int clbptEnqueueSearches(
                 clbpt_tree tree, 
 				int num_keys, 
 				CLBPT_KEY_TYPE *keys, 
-				void *records)					                                {
-	int i, err                                                                  ;
-	for( i = 0 ; i < num_keys ; i++ )                                           {
+				void *records)					{
+	int i, err                                                              ;
+	for( i = 0 ; i < num_keys ; i++ )                                       {
 		err = clbptEnqueueFecthBuffer(	
                 tree, 
-				CLBPT_PACKET_SEARCH(keys[i]), 
-				records)			                                            ;
+		CLBPT_PACKET_SEARCH(keys[i]), 
+		records)			                                ;
         if( err != CLBPT_SUCCESS ) return err                                   ;}
     return CLBPT_SUCCESS                                                        ;}
 
@@ -124,12 +124,12 @@ int clbptEnqueueRangeSearches(
                 CLBPT_KEY_TYPE *l_keys, 
                 CLBPT_KEY_TYPE *u_keys, 
                 void **record_list)                                             {
-	int i, err                                                                  ;
-	for( i = 0 ; i < num_keys ; i++ )                                           {
+	int i, err                                                              ;
+	for( i = 0 ; i < num_keys ; i++ )                                       {
 		err = clbptEnqueueFecthBuffer(
-			    tree, 
-			    CLBPT_PACKET_RANGE(l_keys[i],u_keys[i]), 
-			    (void *)record_list)    	                            		;
+		tree, 
+		CLBPT_PACKET_RANGE(l_keys[i],u_keys[i]), 
+		(void *)record_list)		    	                        ;
         if( err != CLBPT_SUCCESS ) return err                                   ;}
     return CLBPT_SUCCESS                                                        ;}
 
@@ -137,13 +137,13 @@ int clbptEnqueueInsertions(
                 clbpt_tree tree, 
 				int num_inserts, 
 				CLBPT_KEY_TYPE *keys, 
-				void *records)				                                	{
-	int i, err                                                                  ;
-	for( i = 0 ; i < num_inserts ; i++ )                                        {
+				void *records)				        {
+	int i, err                                                              ;
+	for( i = 0 ; i < num_inserts ; i++ )                                    {
 		err = clbptEnqueueFecthBuffer(	
                 tree, 
-				CLBPT_PACKET_INSERT(keys[i],0),
-				records)        			                                    ;
+		CLBPT_PACKET_INSERT(keys[i],0),
+		records)        			                        ;
         if( err != CLBPT_SUCCESS ) return err                                   ;}
     return CLBPT_SUCCESS                                                        ;}
 
@@ -151,12 +151,12 @@ int clbptEnqueueDeletions(
         clbpt_tree tree, 
         int num_deletes, 
         CLBPT_KEY_TYPE *keys)                                                   {
-	int i, err                                                                  ;
-	for( i = 0 ; i < num_deletes ; i++ )                                        {
+	int i, err                                                              ;
+	for( i = 0 ; i < num_deletes ; i++ )                                    {
 		err = clbptEnqueueFecthBuffer(	
                 tree, 
-				CLBPT_PACKET_DELETE(keys[i]), 
-				NULL)        				                                    ;
+		CLBPT_PACKET_DELETE(keys[i]), 
+		NULL)		        				        ;
         if( err != CLBPT_SUCCESS ) return err                                   ;}
     return CLBPT_SUCCESS                                                        ;}
 
