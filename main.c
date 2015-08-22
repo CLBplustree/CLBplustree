6afd97054292444da8eab63a96a44e54d4581deb
mangokingTW <yench@cs.nctu.edu.tw>
2015-08-22 16:41:37 +0800

    our main.c is out of date

--- main.c
+++ main.c
@@ -70,6 +70,77 @@ int main()
 	clbptCreateTree(&t, p, 256, sizeof(int));
 	fprintf(stderr, "CreateTree SUCCESS\n");
 	
+	// read test data
+#define CLBPT_STOP_TYPE 0
+#define CLBPT_INSERT_TYPE 1
+#define CLBPT_SEARCH_TYPE 2
+#define CLBPT_DELETE_TYPE 3
+#define CLBPT_RANGE_TYPE 4
+
+	FILE *input_data ;
+	if(fopen_s(&input_data,"C:\\Users\\mangoking\\Desktop\\CLBPTbench\\input","r+b"))printf("fuck\n");
+	int bfsize = 128;
+	int *data_buffer = calloc(128, sizeof(int));
+	int *rec_buffer = calloc(128, sizeof(int));
+	time_t start_time;
+	time_t end_time;
+	while(1){
+		int i;
+		int err = CL_SUCCESS;
+		int data_info[2] = {0,0};
+		fread(data_info, 2, sizeof(int), input_data);
+		printf("Info %d %d\n",data_info[0],data_info[1]);
+		getchar();
+		time(&start_time);
+		if(!data_info[0] ) break;
+		if (data_info[1] > bfsize){
+			bfsize = data_info[1];
+			data_buffer = realloc(data_buffer, bfsize*sizeof(int));
+			rec_buffer = realloc(rec_buffer, bfsize*sizeof(int));
+		}
+		switch (data_info[0]){
+		case  CLBPT_SEARCH_TYPE:
+			fread(data_buffer, data_info[1], sizeof(int), input_data);
+			for (i = 0; i < data_info[1]; i++){
+				printf("%d key : %d.\n", i, data_buffer[i]);
+			}
+			clbptEnqueueSearches(t, data_info[1], data_buffer, rec_buffer);
+			break;
+		case  CLBPT_INSERT_TYPE:
+			fread(data_buffer, data_info[1], sizeof(int), input_data);
+			fread(rec_buffer, data_info[1], sizeof(int), input_data);
+			//for (i = 0; i < data_info[1]; i++){
+				//printf("%d key : %d.\n", i, data_buffer[i]);
+			//}
+			//getchar();
+			clbptEnqueueInsertions(t, data_info[1], data_buffer, rec_buffer);
+			break;
+		case  CLBPT_DELETE_TYPE:
+			fread(data_buffer, data_info[1], sizeof(int), input_data);
+			for (i = 0; i < data_info[1]; i++){
+				printf("%d key : %d.\n", i, data_buffer[i]);
+			}
+			clbptEnqueueDeletions(t, data_info[1], data_buffer, rec_buffer);
+			break;
+		default: exit(-1);
+		}
+		if (err != CL_SUCCESS)
+		{
+			fprintf(stderr, "EnqueInsertions ERROR\n");
+			exit(err);
+		}
+		clbptFinish(t);
+		fflush(0);
+		if (data_info[0] == CLBPT_SEARCH_TYPE)
+			for (i = 0; i < data_info[1]; i++)
+				printf("key(%d) : %d\n", i, rec_buffer[i]);
+		time(&end_time);
+		int delay = difftime(end_time, start_time);
+		printf("Cost time : %d seconds\n",delay);
+	}
+	return 0;
+	//===========================
+
 	int d[10] = { 7,21,24,23,14,15,16,17,18,19 };
 	int d_rec[10] = { 8,22,25,24,15,16,17,18,19,20 };
 	err = clbptEnqueueInsertions(t, 1, d, d_rec);
